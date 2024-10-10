6d186c8f84a7449e1dbde7dd1524b6eddae2a8a3
soeun823 <bsoeun866@gmail.com>
2024-10-10 15:07:15 +0900

    c언어)1862

---
+++ codeUp/1862.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int n;
+
+int chk[110];
+
+long long d[110];
+
+long long f(int k)
+{
+    if(chk[k] == 1) return d[k];
+    chk[k]=1;
+    if(k <= 2) return d[k]=1;
+    return d[k]=f(k-2)+f(k-1);
+}
+
+int main()
+{
+    scanf("%d", &n);
+    printf("%lld\n", f(n));
+    return 0;
+}
+
+
+
