d1b433feeacf52ff71d8dd30d8a5a50f60412c1c
soeun823 <bsoeun866@gmail.com>
2024-10-17 15:46:46 +0900

    c언어)1856, 메모이제이션

--- codeUp/1856.c
+++ codeUp/1856.c
@@ -1,13 +1,29 @@
+// #include <stdio.h>
+// int up(int n) {
+//     if(n<1) return 0;
+//     else if(n==1) return 1;
+//     else return up(n-1) + up(n-2) + up(n-3);
+// }
+// int main()
+// {
+//     int n;
+//     scanf("%d",&n);
+//     printf("%d",up(n+1));
+//     return 0;
+// }
+
 #include <stdio.h>
+
+int memo[26] = {0,1,2,4};
+
 int up(int n) {
-    if(n<1) return 0;
-    else if(n==1) return 1;
-    else return up(n-1) + up(n-2) + up(n-3);
+    if(memo[n]) return memo[n];
+    return memo[n] = up(n-1) + up(n-2) + up(n-3);
 }
 int main()
 {
     int n;
     scanf("%d",&n);
-    printf("%d",up(n+1));
+    printf("%d",up(n));
     return 0;
 }
